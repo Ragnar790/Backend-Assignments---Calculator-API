2d2ad14bb876ff42b084b923da331fffde5ddc5e
Ragnar790 <kaucykroy@gmail.com>
2020-12-04 19:09:56 +0530

    done. changed the names of result key

--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
-app.get("/home", (req, res) => {
+app.get("", (req, res) => {
 	res.send("Hello world!");
 });
 
 const checkFlow = (num1, num2) => {
-	if (num1 < -1000000 || num2 < -1000000) {
+	if (num1 < -1000000 || num2 < -1000000 || num1 - num2 < -1000000) {
 		return "Underflow";
-	} else if (num1 > 1000000 || num2 > 1000000) {
+	} else if (num1 > 1000000 || num2 > 1000000 || num1 + num2 > 1000000) {
 		return "Overflow";
 	} else if (isNaN(num1) || isNaN(num2)) {
 		return "Invalid data types";
@@ -50,7 +50,7 @@ app.post("/sub", (req, res) => {
 		res.send({
 			status: "success",
 			message: "the difference of given two numbers",
-			sum: req.body.num1 - req.body.num2,
+			difference: req.body.num1 - req.body.num2,
 		});
 	} else {
 		res.send({
@@ -67,7 +67,7 @@ app.post("/multiply", (req, res) => {
 		res.send({
 			status: "success",
 			message: "the product of given numbers",
-			sum: req.body.num1 * req.body.num2,
+			result: req.body.num1 * req.body.num2,
 		});
 	} else {
 		res.send({
@@ -84,7 +84,7 @@ app.post("/divide", (req, res) => {
 		res.send({
 			status: "success",
 			message: "the division of given numbers",
-			sum: req.body.num1 / req.body.num2,
+			result: req.body.num1 / req.body.num2,
 		});
 	} else if (req.body.num2 === 0) {
 		res.send({
